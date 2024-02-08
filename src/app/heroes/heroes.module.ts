91861ca8136bb335b15d56a3ecfdf9fefc513126
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-02-08 15:07:17 -0500

    feat: :construction: Creation of required pages in the Heroes module

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+import { LayotutPageComponent } from './pages/layotut-page/layotut-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroPageComponent,
+    LayotutPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent
+  ],
   imports: [
     CommonModule,
     HeroesRoutingModule
---
+++ src/app/heroes/pages/layotut-page/layotut-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layotut-page',
+  templateUrl: './layotut-page.component.html',
+  styles: [
+  ]
+})
+export class LayotutPageComponent {
+
+}
